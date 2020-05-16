e7c59ce53add1817211c52f68a7d3ceb8064b14d
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-16 12:20:15 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+#  before_action :baria_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user,only: [:edit,:update,:destroy]
   def show
     @book = Book.new
   	@onebook = Book.find(params[:id])
@@ -31,6 +33,7 @@ class BooksController < ApplicationController
 
   def update
   	@book = Book.find(params[:id])
+    @book.user_id = current_user.id
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
@@ -38,7 +41,7 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
   	@book.destoy
   	redirect_to books_path, notice: "successfully delete book!"
@@ -49,6 +52,21 @@ class BooksController < ApplicationController
   def book_params
   	params.require(:book).permit(:title, :body)
   end
+
+      #url直接防止　メソッドを自己定義してbefore_actionで発動。
+  #def baria_user
+   # unless params[:id].to_i == current_user.id
+    #  redirect_to books_path
+   # end
+  #end
+
+      def ensure_correct_user
+      @book = Book.find(params[:id])
+      if @book.user_id != current_user.id
+        redirect_to books_path
+      end
+    end
+
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :baria_user, only: [:edit, :update, :destroy]
+  #before_action :baria_user, only: [:edit, :update]
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "successfully updated user!"
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -31,11 +32,20 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  #url直接防止　メソッドを自己定義してbefore_actionで発動。
-  def baria_user
-    unless params[:id].to_i == current_user.id then
+
+
+  def correct_user
+      user = User.find(params[:id])
+      if current_user != user
       redirect_to user_path(current_user)
     end
   end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+  #def baria_user
+   # unless params[:id].to_i == current_user.id
+    #  redirect_to user_path(current_user)
+    #end
+  #end
 end
 
