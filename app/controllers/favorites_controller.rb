37c41000734e953973dde234393b5b89ced243d4
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-17 12:17:14 +0000

    modiefied

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,15 @@ class FavoritesController < ApplicationController
 	  book = Book.find(params[:book_id]) #current_userだけだと、数がカウントされない
 	  favorite = Favorite.new(user_id: current_user.id, book_id: book.id)
 	  favorite.save
-	  redirect_to book_path(book)
+	  redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 	  book = Book.find(params[:book_id])
 	  favorite = current_user.favorites.find_by(book_id: book.id)
 	  favorite.destroy
-	  redirect_to book_path(book)
+	  redirect_back(fallback_location: root_path)
+	  # request.referer
 	end
 
 
